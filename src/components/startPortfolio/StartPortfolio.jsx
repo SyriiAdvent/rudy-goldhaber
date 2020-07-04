256e79328c826a3dd3b094eeac6e2c17d967fbf5
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-03 23:23:36 -0700

    added horizontal grid and animations

--- src/components/startPortfolio/StartPortfolio.jsx
+++ src/components/startPortfolio/StartPortfolio.jsx
@@ -27,9 +27,6 @@ const StartPortfolio = () => {
   }
 
   const gridCreator = (height = 0, width = 0) => {
-    const xGrid = width / 20
-    const yGrid = height / 20
-
     const xArray = []
     const yArray = []
 
@@ -51,13 +48,18 @@ const StartPortfolio = () => {
   }
 
   const Loading = () => {
+    // anime({
+    //   targets: 'staggering-grid-demo .el',
+    //   scale: [
+    //     {value: .1, easing: 'easeOutSine', duration: 500},
+    //     {value: 1, easing: 'easeInOutQuad', duration: 1200}
+    //   ],
+    //   delay: anime.stagger(200, {grid: [windowSize.width, windowSize.height], from: 'center'})
+    // });
+
     anime({
-      targets: 'staggering-grid-demo .el',
-      scale: [
-        {value: .1, easing: 'easeOutSine', duration: 500},
-        {value: 1, easing: 'easeInOutQuad', duration: 1200}
-      ],
-      delay: anime.stagger(200, {grid: [windowSize.width, windowSize.height], from: 'center'})
+      targets: '.grid .el',
+      rotate: '1turn'
     });
   }
 
@@ -71,12 +73,66 @@ const StartPortfolio = () => {
     }
   }, [windowSize.height, windowSize.width])
 
+  useEffect(() => {
+    setTimeout(() => {
+      anime({
+        targets: '.grid .el',
+        scaleX: 1.2,
+        scaleY: 1.2,
+        backgroundColor: '#000',
+        duration: 1000,
+        zIndex: 20
+        
+      }).finished.then(() => {
+        anime({
+          targets: '.grid .els',
+          scaleX: 0,
+          scaleY: 0,
+          rotate: '1turn',
+          duration: 2000,
+          easing: 'linear'
+        })
+        .finished.then(() => {
+          anime({
+            targets: '.grid .el',
+            scaleX: 0,
+            scaleY: 0,
+            rotate: '1turn',
+            duration: 2000,
+            easing: 'linear'
+          })
+        })
+      })
+      // anime({
+      //   targets: '.grid .el',
+      //   scaleX: 0,
+      //   scaleY: 0,
+      //   rotate: '1turn',
+      //   duration: 2000,
+      //   easing: 'linear'
+      // })
+      // .finished.then(() => {
+      //   anime({
+      //     targets: '.grid .els',
+      //     scaleX: 0,
+      //     scaleY: 0,
+      //     rotate: '1turn',
+      //     duration: 2000,
+      //     easing: 'linear'
+      //   });
+      // })
+
+    }, 1500);
+  }, [])
+
   console.log("window size: ", windowSize.width, windowSize.height, grid);
 
   return (
     <LoadContainer>
       <div className='grid'>
-        {windowSize.height > 0 ? grid.verticalGrid.map(node => ( <div className='small square el'></div>)) : null}
+        {windowSize.width && windowSize.height > 0 ? grid.verticalGrid.map(node => {
+          return grid.horizontalGrid.map((node, index) => (<div className={index % 2 === 0 ? 'small square els' : 'small square el'}></div>))
+        }) : null}
       </div>
       {/* <button onClick={() => Loading()}>Init</button> */}
     </LoadContainer>
