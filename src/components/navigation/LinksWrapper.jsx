909daada32876fca6ca4c2fa1979ce68eceec38f
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-05 12:13:25 -0700

    add resume download

--- src/components/navigation/LinksWrapper.jsx
+++ src/components/navigation/LinksWrapper.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import cv from '../../assets/Rudy Goldhaber CV.pdf'
 
 const LinkContainer = styled.ul`
   margin: 0;
   padding: 0;
-  width: 50%;
+  width: 40%;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
@@ -16,6 +17,11 @@ const LinkContainer = styled.ul`
     text-decoration: none;
     list-style: none;
     cursor: pointer;
+
+    a {
+      text-decoration: none;
+      color: #fff;
+    }
   }
 
   @media (max-width: 786px) {
@@ -34,8 +40,8 @@ const LinksWrapper = props => {
     <LinkContainer>
       <li onClick={() => props.refs.scrollToAbout()}>About</li>
       <li onClick={() => props.refs.scrollToProjects()}>Projects</li>
-      <li>Contact</li>
-      <Resume>Resume</Resume>
+      {/* <li>Contact</li> */}
+      <Resume><a href={cv} target='_blank'>Resume</a></Resume>
     </LinkContainer>
   )
 }
