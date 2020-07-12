5c9695802405e6244fcb6dd03ff041ea3274fba4
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-12 10:27:57 -0700

    change node anim time in half, less perf cost

--- src/components/startPortfolio/StartPortfolio.jsx
+++ src/components/startPortfolio/StartPortfolio.jsx
@@ -65,7 +65,7 @@ const StartPortfolio = props => {
     anime({
       targets: '.stagger-grid .el',
       scale: [
-        {value: 0, easing: 'easeOutSine', duration: 800},
+        {value: 0, easing: 'easeOutSine', duration: 400},
         // {value: 1, easing: 'easeInOutQuad', duration: 1200}
       ],
       delay: anime.stagger(50, { 
