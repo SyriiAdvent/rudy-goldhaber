5dad81384ae012a4a77b3d9f15abf8a5c612b839
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-12 17:10:08 -0700

    change to fade animation for less eye strain

--- src/components/projects/ImageCard.jsx
+++ src/components/projects/ImageCard.jsx
@@ -17,7 +17,6 @@ import anime from 'animejs'
 const ImageCardContainer = styled.div`
   width: 32rem;
   height: 25rem;
-  
 `
 
 const FloatingProjectImg = styled.div`
@@ -38,11 +37,11 @@ const ImageCard = props => {
 
   const imageAnimate = () => {
     return anime({
-      targets: (`${direction}`) === 'reverse' ? '.reverse' : '.animate-image',
-      translateX: (`${direction}`) === 'reverse' ? 1000 : -1000,
-      direction: 'alternate',
-      easing: 'easeInOutSine',
-      duration: 500
+      targets: '.animate-image',
+      opacity: [
+        {value: 0, easing: 'linear', duration: 700},
+        {value: 1, easing: 'linear', duration: 700}
+      ],
     })
   }
 
@@ -52,7 +51,7 @@ const ImageCard = props => {
         imageAnimate()
         setTimeout(() => {
           setTransitioned(transitioned + 1);
-        }, 250)
+        }, 700)
       }, 5000)
     } 
     else {
@@ -78,7 +77,7 @@ const ImageCard = props => {
 
   return (
     <ImageCardContainer>
-      <FloatingProjectImg className={'animate-image ' + props.direction} imgUrl={currentImage[transitioned]} />
+      <FloatingProjectImg className={'animate-image'} imgUrl={currentImage[transitioned]} />
     </ImageCardContainer>
   )
 }
